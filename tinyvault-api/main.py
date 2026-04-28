f04e9aae07e8acad050097395b2e09e1e59c4184
yusufrmak <Yusufrmak497@icloud.com>
2026-04-28 15:40:04 +0300

    Allow Netlify origin in CORS

--- tinyvault-api/main.py
+++ tinyvault-api/main.py
@@ -64,6 +64,7 @@ ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:8000",
+    "https://subtrack1232.netlify.app",
     "chrome-extension://",  # Chrome extension support
 ]
 app.add_middleware(
